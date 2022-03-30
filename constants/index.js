ef037a084937d2668bc31efb4d6acce49d0bef13
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-03-30 14:24:37 -0700

    creates deploy script

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x83232F692758f81569cE2387340955cb30E3c21A";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
