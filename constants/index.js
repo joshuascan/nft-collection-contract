5048d23defd34fabe8a6e7dd902442bf5229c200
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-03-31 13:26:41 -0700

    updates metadata url

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x814C8E134e6deE95F4F9E1DbC5f0Fb3e14c5A86f";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-azure.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
